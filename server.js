f4b47396d1086c89f3b32cee25f63ed30c85175e
Solomon sokoya <solomonsokoya@ool-ad0202c0.dyn.optonline.net>
2018-05-31 15:35:37 -0400

    Made change to server

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var io = require('socket.io')(server);
 server.listen(3000);
 
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.send("hii")
+  // res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
