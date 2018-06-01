5c88be9ab1ebc859870a52f66437c784f0af977c
Solomon sokoya <solomonsokoya@ool-ad0202c0.dyn.optonline.net>
2018-06-01 15:56:08 -0400

    Changed to server .js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (req, res) {
 io.on('connection', function (socket) {
   console.log("This is socket.id:" + socket.id )
   socket.on('chat message', function(msg){
-     socket.emit("message" , msg)
+     io.emit("chat message" , msg)
     console.log('message: ' + msg);
   })
 
